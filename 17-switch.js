7da4644ac68568fe79d47394e820d0572a90e70e
RITURAJ-ARENA <majrituraj@gmail.com>
2026-05-06 03:30:43 +0530

    Switch Statements

---
+++ 17-switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("Febuary")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break;
+
+    default:
+        console.log("Nothing Matched")
+        break;
+}
+
+// If any switch statement matches the condition,
+// after that switch executes all the statements., that's why Break is Imp.
